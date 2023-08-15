63f4a0db7d95f2416e718d8946228e769265faf2
rizkisiraj <jomirwa@gmail.com>
2023-08-15 08:29:33 +0700

    Initial Commit

---
+++ MealMinder/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MealMinder
+//
+//  Created by Muhammad Rizki Siraj on 15/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MealMinder/MealMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealMinderApp.swift
+//  MealMinder
+//
+//  Created by Muhammad Rizki Siraj on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MealMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
